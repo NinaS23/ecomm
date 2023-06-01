6e886752a609a9eb1abd280a111f83d950f4ffc5
Maria <mariabevenuto062@gmail.com>
2023-06-01 16:26:12 -0300

    feat: listaProdutosDeCategorias.js

---
+++ mongo/ listaProdutosDeCategorias.js
@@ -0,0 +1,10 @@
+use("ecomm")
+
+const productsCategoriesList = db.categories.find({
+    $or: [
+        { categoria: "LIVROS" },
+        { categoria: "CELULARES" }
+    ]
+});
+
+console.log(productsCategoriesList);
