df6595973bbc722f5485b31b6f09923203bdec7b
Maria <mariabevenuto062@gmail.com>
2023-06-01 16:26:44 -0300

    feat: criarIndiciesProducts.js

---
+++ mongo/criarIndicesProducts.js
@@ -0,0 +1,8 @@
+use("ecomm")
+
+const createdIndexName = db.products.createIndex({ nome: 1 });
+const createdIndexCategory = db.products.createIndex({ categoria: 1 });
+const createIndexPrice = db.products.createIndex({ preco: 1 });
+
+console.log(createdIndexCategory,createdIndexName,createIndexPrice);
+
