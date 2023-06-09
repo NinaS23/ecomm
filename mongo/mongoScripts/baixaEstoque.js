ff5392eb8e201037b0d699994cf1a442b4913652
NinaS23 <mariabevenuto062@gmail.com>
2023-06-09 14:53:18 -0300

    feat: baixaEstoque with a simple format

--- mongo/mongoScripts/baixaEstoque.js
+++ mongo/mongoScripts/baixaEstoque.js
@@ -1,49 +1,9 @@
 use("ecomm")
 
-let DataDoPedido = new Date();
-const produtoQueFoiPedido = {
-    productId: new ObjectId("648336b5dddea3f9a30b9d83"),
-    quantidade: 1,
-    precoUnitario: NumberDecimal("102.9")
-}
+const produtoModificado = db.products.updateOne(
+    { nome: "Galaxy Tab S8" },
+    {gte: 2 },
+    { $inc: { estoque: -2 } }
+)
 
-const pedido = {
-    "dataPedido": DataDoPedido,
-    "enderecoEntrega": {
-        "bairro": "Centro",
-        "rua": "Avenida Floriano Gonçalves de Lima 93",
-        "numero": "1549",
-        "complemento": "em frente ao pet shop xero-bom",
-        "cep": "55555970",
-        "cidade": "Xexéu",
-        "uf": "PE"
-    },
-    "account": {
-        "accountId": ObjectId("6480f55bcc81d477fda96440"),
-        "cliente": "Emilia Dos Santos"
-    },
-    "itens":[produtoQueFoiPedido]
-};
-
-
-const verificandoQuantidadeDoProduto = {
-    _id: new ObjectId("648336b5dddea3f9a30b9d83"),
-    estoque:
-    {
-        $gte: produtoQueFoiPedido.quantidade
-    }
-};
-
-const diminuirEstoque = {
-    $inc: {
-        estoque: - ( produtoQueFoiPedido.quantidade)
-    }
-};
-
-const mostraUpdate = db.products.updateOne(
-    verificandoQuantidadeDoProduto,
-    diminuirEstoque
-);
-db.orders.insertOne(pedido);
-
-console.log(mostraUpdate)
+console.log(produtoModificado)
