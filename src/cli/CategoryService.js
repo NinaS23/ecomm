b3cc94c4f2f678c8dc50f631da9797d0a086f4c8
NinaS23 <mariabevenuto062@gmail.com>
2023-06-15 16:40:01 -0300

    fix: functions for class

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -31,6 +31,7 @@ function renderizaStatus(statusCode, method) {
 	}
 }
 
+
 function renderizaDados(dados) {
 	const dadosResolvidos = Promise.resolve(dados);
 	dadosResolvidos.then((value) => {
@@ -38,127 +39,121 @@ function renderizaDados(dados) {
 	});
 
 }
-
-function encontraCategorias() {
-	return new Promise((resolve, reject) => {
-		fetch("http://localhost:3000/categories")
-			.then((response) => {
-				renderizaStatus(response.status, "get");
-				return response.json();
-			})
-			.then((data) => {
-				setTimeout(() => {
-					renderizaDados(data);
-				}, 2000);
-			})
-			.catch((error) => {
-				reject(trataErro(error));
-			});
-	});
-}
-
-function econtraCategoriaPeloId(id) {
-	let idDaCategoriaDesejada = id;
-	return new Promise((_, reject) => {
-		fetch("http://localhost:3000/categories")
-			.then((response) => {
-				renderizaStatus(response.status, "get");
-				return response.json();
-			})
-			.then((data) => {
-				const categorias = data;
-				categorias.find((categoria) => {
-					if (categoria.id === idDaCategoriaDesejada) renderizaDados(categoria);
+class categoryService {
+	static encontraCategorias() {
+		return new Promise((resolve, reject) => {
+			fetch("http://localhost:3000/categories")
+				.then((response) => {
+					renderizaStatus(response.status, "get");
+					return response.json();
+				})
+				.then((data) => {
+					setTimeout(() => {
+						renderizaDados(data);
+					}, 2000);
+				})
+				.catch((error) => {
+					reject(trataErro(error));
 				});
+		});
+	}
 
+	static econtraCategoriaPeloId(id) {
+		let idDaCategoriaDesejada = id;
+		return new Promise((_, reject) => {
+			fetch("http://localhost:3000/categories")
+				.then((response) => {
+					renderizaStatus(response.status, "get");
+					return response.json();
+				})
+				.then((data) => {
+					const categorias = data;
+					categorias.find((categoria) => {
+						if (categoria.id === idDaCategoriaDesejada) renderizaDados(categoria);
+					});
+
+				})
+				.catch((error) => {
+					reject(trataErro(error));
+				});
+		});
+	}
+
+	static async criarCategoria() {
+		const caminhoDaCategoriaNova = "./src/cli/novaCategoria.json";
+		const encoding = "utf-8";
+		const categoria = await fs.promises.readFile(caminhoDaCategoriaNova, encoding);
+		const headers = {
+			"Content-Type": "application/json"
+		};
+		let requestOptions = {
+			method: "POST",
+			headers: headers,
+			body: categoria
+		};
+		fetch("http://localhost:3000/categories", requestOptions)
+			.then((response) => {
+				renderizaStatus(response.status);
+				return response.json();
+			})
+			.then((data) => {
+				renderizaDados(data);
 			})
 			.catch((error) => {
-				reject(trataErro(error));
+				trataErro(error);
 			});
-	});
-}
+	}
 
-async function criarCategoria() {
-	const caminhoDaCategoriaNova = "./src/cli/novaCategoria.json";
-	const encoding = "utf-8";
-	const categoria = await fs.promises.readFile(caminhoDaCategoriaNova, encoding);
-	const headers = {
-		"Content-Type": "application/json"
-	};
-	let requestOptions = {
-		method: "POST",
-		headers: headers,
-		body: categoria
-	};
-	fetch("http://localhost:3000/categories", requestOptions)
-		.then((response) => {
-			renderizaStatus(response.status);
-			return response.json();
-		})
-		.then((data) => {
-			renderizaDados(data);
-		})
-		.catch((error) => {
-			trataErro(error);
-		});
-}
+	static async atualizaCategoria(idDaCategoria) {
+		let categoriaId = idDaCategoria;
+		const caminhoDaCategoriaNova = "./src/cli/categoriaAtualizada.json";
+		const encoding = "utf-8";
+		const atualizarData = await fs.promises.readFile(caminhoDaCategoriaNova, encoding);
+		const headers = {
+			"Content-Type": "application/json"
+		};
+		let requestOptions = {
+			method: "PATCH",
+			headers: headers,
+			body: atualizarData
+		};
+		fetch(`http://localhost:3000/categories/${categoriaId}`, requestOptions)
+			.then((response) => {
+				renderizaStatus(response.status, "patch");
+				return response.json();
+			})
+			.then((data) => {
+				renderizaDados(data);
+			})
+			.catch((error) => {
+				trataErro(error);
+			});
+	}
 
-async function atualizaCategoria(idDaCategoria) {
-	let categoriaId = idDaCategoria;
-	const caminhoDaCategoriaNova = "./src/cli/categoriaAtualizada.json";
-	const encoding = "utf-8";
-	const atualizarData = await fs.promises.readFile(caminhoDaCategoriaNova, encoding);
-	const headers = {
-		"Content-Type": "application/json"
-	};
-	let requestOptions = {
-		method: "PATCH",
-		headers: headers,
-		body: atualizarData
-	};
-	fetch(`http://localhost:3000/categories/${categoriaId}`, requestOptions)
-		.then((response) => {
-			renderizaStatus(response.status, "patch");
-			return response.json();
-		})
-		.then((data) => {
-			renderizaDados(data);
-		})
-		.catch((error) => {
-			trataErro(error);
-		});
-}
-
-async function deletarCategoria(idDaCategoria) {
-	let categoriaId = idDaCategoria;
-	const headers = {
-		"Content-Type": "application/json"
-	};
-	let requestOptions = {
-		method: "DELETE",
-		headers: headers,
-	};
-	fetch(`http://localhost:3000/categories/${categoriaId}`, requestOptions)
-		.then((response) => {
-			renderizaStatus(response.status, "delete");
-			return response.json();
-		})
-		.then((data) => {
-			renderizaDados(data);
-		})
-		.catch((error) => {
-			trataErro(error);
-		});
+	static async deletarCategoria(idDaCategoria) {
+		let categoriaId = idDaCategoria;
+		const headers = {
+			"Content-Type": "application/json"
+		};
+		let requestOptions = {
+			method: "DELETE",
+			headers: headers,
+		};
+		fetch(`http://localhost:3000/categories/${categoriaId}`, requestOptions)
+			.then((response) => {
+				renderizaStatus(response.status, "delete");
+				return response.json();
+			})
+			.then((data) => {
+				renderizaDados(data);
+			})
+			.catch((error) => {
+				trataErro(error);
+			});
+	}
 }
 
 
-const categoryService = {
-	encontraCategorias,
-	econtraCategoriaPeloId,
-	criarCategoria,
-	atualizaCategoria,
-	deletarCategoria
-};
 
 export default categoryService;
 
